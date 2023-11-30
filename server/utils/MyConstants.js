6ffcb1f9adbf7ecb39d4ed0df7ff3d32cd70e011
tran555868877 <62251418+tran555868877@users.noreply.github.com>
2023-11-30 08:55:49 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.aztt1ke.mongodb.net',
+  DB_USER: 'lab1',
+  DB_PASS: 't123456',
+  DB_DATABASE: 'book1',
+  JWT_SECRET: 'caigicungdc',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: '<tran22413862>', // gmail service
+  EMAIL_PASS: 'dcqt muyz ysbf wwyh  '
+};
+module.exports = MyConstants;
